59d3afd0ca0477e8e3827fe678fc472e22b8cceb
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-20 13:46:54 +0100

    fix: Add @JsonFormat to OrderDto to ensure date time formatting

--- apps/order/src/main/java/com/github/thorlauridsen/dto/OrderDto.java
+++ apps/order/src/main/java/com/github/thorlauridsen/dto/OrderDto.java
@@ -1,5 +1,6 @@
 package com.github.thorlauridsen.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.thorlauridsen.enumeration.OrderStatus;
 import com.github.thorlauridsen.model.Order;
@@ -19,7 +20,7 @@ import java.util.UUID;
         example = """
                 {
                     "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                    "time": "2025-03-13T18:39:00.4900802Z",
+                    "time": "2025-03-13T18:39:00Z",
                     "status": "CREATED",
                     "product": "Computer",
                     "amount": 199.0
@@ -28,7 +29,7 @@ import java.util.UUID;
 )
 public record OrderDto(
         @JsonProperty("id") UUID id,
-        @JsonProperty("time") OffsetDateTime time,
+        @JsonProperty("time") @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssX") OffsetDateTime time,
         @JsonProperty("status") OrderStatus status,
         @JsonProperty("product") String product,
         @JsonProperty("amount") double amount
