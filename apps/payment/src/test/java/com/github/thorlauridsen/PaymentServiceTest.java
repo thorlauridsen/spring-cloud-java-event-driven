2896127303b8d9bff265a9255872f6a542047458
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-20 15:10:23 +0100

    test: Add PaymentServiceTest

---
+++ apps/payment/src/test/java/com/github/thorlauridsen/PaymentServiceTest.java
@@ -0,0 +1,47 @@
+package com.github.thorlauridsen;
+
+import com.github.thorlauridsen.event.OrderCreatedEvent;
+import com.github.thorlauridsen.outbox.OutboxRepo;
+import com.github.thorlauridsen.persistence.PaymentRepo;
+import com.github.thorlauridsen.service.PaymentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class PaymentServiceTest {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @Autowired
+    private PaymentRepo paymentRepo;
+
+    @Autowired
+    private OutboxRepo outboxRepo;
+
+    @BeforeEach
+    public void setup() {
+        paymentRepo.deleteAll();
+        outboxRepo.deleteAll();
+        assertEquals(0, paymentRepo.count());
+        assertEquals(0, outboxRepo.count());
+    }
+
+    @Test
+    public void processOrderCreated() {
+        var event = new OrderCreatedEvent(
+                UUID.randomUUID(),
+                "Computer",
+                199.0
+        );
+        paymentService.processOrderCreated(event);
+        assertEquals(1, paymentRepo.count());
+        assertEquals(1, outboxRepo.count());
+    }
+}
