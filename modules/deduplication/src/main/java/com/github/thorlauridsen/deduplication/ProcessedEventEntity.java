906611a5c9d152000a8388268579642b7266d323
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-23 13:46:01 +0100

    docs: Update Javadoc for ProcessedEventEntity

--- modules/deduplication/src/main/java/com/github/thorlauridsen/deduplication/ProcessedEventEntity.java
+++ modules/deduplication/src/main/java/com/github/thorlauridsen/deduplication/ProcessedEventEntity.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+/**
+ * Class representing a processed event.
+ * This database entity will be saved in the "processed_event" table.
+ * The purpose of this entity is to keep track of events that have already been processed.
+ * This is useful for idempotency and to avoid processing the same event multiple times.
+ */
 @Entity
 @Table(name = "processed_event")
 public class ProcessedEventEntity {
@@ -24,6 +29,11 @@ public class ProcessedEventEntity {
     public ProcessedEventEntity() {
     }
 
+    /**
+     * Constructor for ProcessedEventEntity.
+     *
+     * @param eventId UUID of the event.
+     */
     public ProcessedEventEntity(UUID eventId) {
         this.eventId = eventId;
         this.processedAt = OffsetDateTime.now();
