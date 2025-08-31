916c7394047161a183be86f8599ef22ff65b3c1f
Thor Tudal Lauridsen <30320530+thorlauridsen@users.noreply.github.com>
2025-08-31 18:09:01 +0200

    build: Use Kotlin DSL accessors in build.gradle.kts files (#68)
    Take advantage of the features of Kotlin DSL by using accessors in `build.gradle.kts` files.

--- apps/payment/build.gradle.kts
+++ apps/payment/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
 	testRuntimeOnly(local.junit.platform.launcher)
 }
 
-tasks.withType<Test> {
+tasks.test {
 	useJUnitPlatform()
 }
--- modules/deduplication/build.gradle.kts
+++ modules/deduplication/build.gradle.kts
@@ -1,25 +1,22 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-import org.springframework.boot.gradle.tasks.run.BootRun
-
 plugins {
-	alias(local.plugins.lombok)
-	alias(local.plugins.springboot)
-	alias(local.plugins.spring.dependencies)
+    alias(local.plugins.lombok)
+    alias(local.plugins.springboot)
+    alias(local.plugins.spring.dependencies)
 }
 
 dependencies {
-	// The outbox subproject needs access to the model subproject
-	implementation(projects.model)
+    // The outbox subproject needs access to the model subproject
+    implementation(projects.model)
 
-	// Spring Boot dependencies
-	implementation(local.springboot.starter.jpa)
+    // Spring Boot dependencies
+    implementation(local.springboot.starter.jpa)
 }
 
 // Disabling bootJar and bootRun is necessary for a subproject/module
 // that uses the Spring Boot plugin but is not supposed to be executable.
-tasks.named<BootJar>("bootJar") {
-	enabled = false
+tasks.bootJar {
+    enabled = false
 }
-tasks.named<BootRun>("bootRun") {
-	enabled = false
+tasks.bootRun {
+    enabled = false
 }
