f7bd738be4b8ea01f1df9be7b0ccec29978b994c
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-09-16 20:26:03 +0200

    refac: Use Lombok UtilityClass annotation in BaseEndpoint
    Use Lombok UtilityClass annotation in BaseEndpoint because the class is an utility class with only a static constant.

--- apps/payment/src/main/java/com/github/thorlauridsen/controller/BaseEndpoint.java
+++ apps/payment/src/main/java/com/github/thorlauridsen/controller/BaseEndpoint.java
@@ -1,8 +1,11 @@
 package com.github.thorlauridsen.controller;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * This class contains the base endpoint constant for the payment controller.
  */
+@UtilityClass
 public class BaseEndpoint {
     public static final String PAYMENT_BASE_ENDPOINT = "/payments";
 }
