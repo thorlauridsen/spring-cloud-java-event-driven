a8b09ec2bb1246ab6ab96933e5c2fe31ae263bab
Thor Tudal Lauridsen <30320530+thorlauridsen@users.noreply.github.com>
2025-09-16 20:28:50 +0200

    refac: Use Lombok UtilityClass annotation in BaseEndpoint (#73)
    Use Lombok `UtilityClass` annotation in `BaseEndpoint` because the class is an utility class with only a static constant.

--- apps/order/src/main/java/com/github/thorlauridsen/controller/BaseEndpoint.java
+++ apps/order/src/main/java/com/github/thorlauridsen/controller/BaseEndpoint.java
@@ -1,8 +1,11 @@
 package com.github.thorlauridsen.controller;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * This class contains the base endpoint constant for the order controller.
  */
+@UtilityClass
 public class BaseEndpoint {
     public static final String ORDER_BASE_ENDPOINT = "/orders";
 }
